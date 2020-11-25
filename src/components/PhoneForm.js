4e5527d84068f16afa24046dfc5e85c38ed39313
h-yes-oo <hyesoo5115@naver.com>
2020-11-25 09:48:20 +0900

    dom focus by ref

--- src/components/PhoneForm.js
+++ src/components/PhoneForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 export default class PhoneForm extends Component {
+    input = React.createRef();
 
     state = {
         name: '',
@@ -16,14 +17,12 @@ export default class PhoneForm extends Component {
     handleSubmit = (e) => {
         //기본으로 수행되는 새로고침을 방지함
         e.preventDefault();
-        this.props.onCreate({
-            name: this.state.name,
-            phone: this.state.phone
-        })
+        this.props.onCreate(this.state);
         this.setState({
             name: '',
             phone: ''
-        })
+        });
+        this.input.current.focus();
     }
     render() {
         return (
@@ -33,6 +32,7 @@ export default class PhoneForm extends Component {
                     placeholder="이름"
                     onChange={this.handleChange} 
                     value={this.state.name} 
+                    ref={this.input}
                 />
                 <input 
                     name="phone"
