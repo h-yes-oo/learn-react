f8504acc34735fde01092115330b4a7103d2b543
h-yes-oo <hyesoo5115@naver.com>
2020-12-28 12:39:46 +0900

    useEffect, useMemo

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,10 +1,15 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useMemo, useCallback } from 'react';
 import Hello from './Hello';
 import Counter from './Counter';
 import InputSample from './InputSample';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 import './App.css';
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중..');
+  return users.filter(user => user.active).length;
+}
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -67,6 +72,8 @@ function App() {
     );
   };
 
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
   return (
     <>
       <CreateUser 
@@ -76,6 +83,7 @@ function App() {
         onCreate = {onCreate}
       />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성사용자 수 : {count}</div>
     </>
   );
 }
--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function User({ user, onRemove, onToggle }) {
+    // useEffect(() => {
+    //     console.log(user);
+    // })
     return (
         <div>
             <b
@@ -9,7 +12,9 @@ function User({ user, onRemove, onToggle }) {
                     color: user.active ? 'green' : 'black'
                 }}
                 onClick={() => onToggle(user.id)}
-            >{user.username}</b> 
+            >
+                {user.username}
+            </b> 
             &nbsp;
             <span>({user.email})</span>
             <button onClick={() => onRemove(user.id)}>삭제</button>
