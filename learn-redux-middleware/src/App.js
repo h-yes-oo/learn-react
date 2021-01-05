4fff67eb4000ceaae4d3677b57b245b223d84f69
h-yes-oo <hyesoo5115@naver.com>
2021-01-05 20:13:35 +0900

    redux-thunk with Promise

--- learn-redux-middleware/src/App.js
+++ learn-redux-middleware/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import CounterContainer from './containers/CounterContainer';
+import PostListContainer from './containers/PostListContainer';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
 
 function App() {
-  return <CounterContainer />;
+  return (
+    <>
+      <Route path="/" component={PostListPage} exact={true} />
+      <Route path="/:id" component={PostPage} />
+    </>
+  
+    )
 }
 
 export default App;
